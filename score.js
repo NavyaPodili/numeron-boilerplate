918b61a69116ca5574b804b8adc2d1c3ba3836e0
NavyaPodili <160600065+NavyaPodili@users.noreply.github.com>
2024-03-20 18:56:33 +0530

    Update score.js

--- score.js
+++ score.js
@@ -1,1 +1,15 @@
 // Iteration 5: Store the player score and display it on the game over screen
+document.addEventListener('DOMContentLoaded', function() {
+    var scoreBoard = document.getElementById("score-board");
+    var playerScore = localStorage.getItem('playerScore');
+    console.log("Player's score:", playerScore);
+    scoreBoard.textContent = playerScore;
+});
+play_again_button=document.getElementById("play-again-button");
+play_again_button.addEventListener("click", function()
+{
+  window.location.href = "game.html";
+});
+
+  
+
