79971e37f1c6b200ca251b600e909cca5ed35bb2
NavyaPodili <160600065+NavyaPodili@users.noreply.github.com>
2024-03-20 18:55:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
 // Iteration 1: Make the Play button functional.
 // Description: When the Play button is clicked the game page should be displayed.
+document.addEventListener('DOMContentLoaded', function() {
+    var buttons = document.getElementById("buttons").getElementsByTagName("img");
+    function handleSymbolButtonClick() {
+        console.log("Button clicked:", this.getAttribute('id'));
+    }
+    for (let button of buttons) {
+        button.addEventListener('click', handleSymbolButtonClick);
+    }
+});
